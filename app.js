35f26dd5f74f777145e7ff0ac5a59bf25c9ff130
Parker King <parkerking@Parkers-MacBook-Pro.local>
2017-06-06 13:47:13 -0600

    Adding the styling to the cipher

--- app.js
+++ app.js
@@ -2,61 +2,68 @@ var app = angular.module("cipherApp", []);
 
 app.controller("mainCtrl", ["$scope", function ($scope, hitService) {
 
+	if($scope.textBox === undefined){
+		$scope.text = true;
+	} 
+	if($scope.textBox !== undefined){
+		$scope.text = false;
+	} 
+	
 	$scope.cipher = function () {
+		var amount = parseInt($scope.selected);
 		var cipher = $scope.textBox;
-		$scope.cipheringText(cipher);
+		$scope.cipheringText(cipher,amount);
 		$scope.textBox = '';
 
 	}
-	$scope.cipheringText = function (str) {
+	$scope.cipheringText = function (str, amount) {
 		var newStr = []
 		for (var i = 0; i < str.length; i++) {
-			newStr.push($scope.switchCipher(str[i]));
+			newStr.push($scope.switchCipher(str[i], amount));
 		}
 		newStr = newStr.join('');
 		$scope.cipheredText = newStr;
 	}
 
-	$scope.switchCipher = function (letter) {
-		if(letter === " "){
+	$scope.switchCipher = function (letter, amount) {
 
+		if(letter === " "){
 			return letter
 		}
 		letter = letter.toLowerCase();
-		var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a']
+		var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 		for(var i = 0; i < alphabet.length; i++){
 			if(letter === alphabet[i]){
-				 return alphabet[i + 1]
+				 return alphabet[i + amount]
 			}
 		}
-		
 	}
 	
 	$scope.decipher = function () {
 		var decipher = $scope.textBox;
-		$scope.decipheringText(decipher);
+		var amount = parseInt($scope.selected);
+		$scope.decipheringText(decipher,amount);
 		$scope.textBox = '';
 
 	}
-	$scope.decipheringText = function (str) {
+	$scope.decipheringText = function (str,amount) {
 		var newStr = []
 		for (var i = 0; i < str.length; i++) {
-			newStr.push($scope.switchDecipher(str[i]));
+			newStr.push($scope.switchDecipher(str[i],amount));
 		}
 		newStr = newStr.join('');
 		$scope.decipheredText = newStr;
 	}
 	
-	$scope.switchDecipher = function (letter) {
+	$scope.switchDecipher = function (letter,amount) {
 		if(letter === " "){
 			return letter
 		}
-
 		letter = letter.toLowerCase();
-		var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a']
+		var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 		for(var i = 0; i < alphabet.length; i++){
 			if(letter === alphabet[i]){
-				 return alphabet[i - 1]
+				 return alphabet[i - amount]
 			}
 		}
 		
